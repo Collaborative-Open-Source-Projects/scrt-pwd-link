01995e1fe75beb8fa2d4b45c14f20cf00f89ff73
Mahmoud Zeus <meropvo17@gmail.com>
2024-09-28 08:05:53 +0300

    change the encryption key to the key in the environment

--- app/utils/encrypt.js
+++ app/utils/encrypt.js
@@ -1,14 +1,21 @@
-import crypto from 'crypto';
+import crypto from "crypto";
 
-const encryptionKey = crypto.randomBytes(32).toString('hex');
+const encryptionKey = process.env.ENCRYPTION_KEY;
+
 const ivLength = 16;
 
-export function encrypt (text) {
+export function encrypt(text) {
+  const iv = crypto.randomBytes(ivLength);
+  const cipher = crypto.createCipheriv(
+    "aes-256-cbc",
+    Buffer.from(encryptionKey, "hex"),
+    iv
+  );
+  let encryptedText = cipher.update(text);
+  encryptedText = Buffer.concat([encryptedText, cipher.final()]);
 
-    const iv = crypto.randomBytes(ivLength);
-    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(encryptionKey, 'hex'), iv);
-    let encryptedText = cipher.update(text);
-    encryptedText = Buffer.concat([encryptedText, cipher.final()]);
-
-    return { iv: iv.toString('hex'), encryptedData: encryptedText.toString('hex') };
+  return {
+    iv: iv.toString("hex"),
+    encryptedData: encryptedText.toString("hex"),
+  };
 }
