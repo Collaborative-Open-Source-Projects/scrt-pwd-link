9dcba2438f6b92bb1806a8eefd3f52466a5769ee
Mahmoud Zeus <meropvo17@gmail.com>
2024-10-01 08:49:29 +0300

    add reusable button component

--- app/components/ui/Button.jsx
+++ app/components/ui/Button.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-
-export default function Button() {
-  return <div>Button</div>;
+/* this reusable component for button
+ 1- takes isMain prop , which implement two different styles 
+ 2- takes children to put the content of the button
+*/
+export default function Button({ isMain = false, children }) {
+  return (
+    <button
+      className={`box-border px-4 h-[36px]  rounded ${
+        isMain ? "hover:bg-teal-700 bg-teal-600" : "text-teal-600 bg-white"
+      }`}
+    >
+      {children}
+    </button>
+  );
 }
