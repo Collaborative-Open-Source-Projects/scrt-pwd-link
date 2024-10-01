41baf8c5555ad6810b0b829e536329e36a23cf42
Mahmoud Zeus <meropvo17@gmail.com>
2024-10-01 08:40:28 +0300

    edit input component and add comments for reuse

--- app/components/ui/Input.jsx
+++ app/components/ui/Input.jsx
@@ -1,10 +1,25 @@
-export default function Input({ width = "w-3/4", islarge = false }) {
+/* this reusable component for input , it takes 5 props
+ 1- width className defined by tailwind , you can use it to set width , width for different screen sizes
+ 2- islarge if you want to set height to 28 for large input
+ 3- label for input
+ 4- placeholder
+ 5- onChange method that takes event as parameter
+*/
+export default function Input({
+  width = "w-3/4",
+  islarge = false,
+  label,
+  placeholder,
+  onChange,
+}) {
   return (
     <div className={`${width}`}>
       <label className="block text-gray-700 text-sm font-bold mb-1 text-start ps-1 ">
-        Secret
+        {label}
       </label>
       <input
+        onChange={(e) => onChange(e)}
+        placeholder={placeholder}
         className={`${
           islarge ? "h-28" : "w-3/4"
         } w-full shadow-xl appearance-none border rounded-lg  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
