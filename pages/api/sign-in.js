258588f36f7ecc635b364bfb28f9f5a6f5d090af
Anuj Gupta <anujgupta4155@gmail.com>
2024-11-16 00:04:17 +0530

    fix: update token payload for consistency during registration and login

--- pages/api/sign-in.js
+++ pages/api/sign-in.js
@@ -1,51 +1,65 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import {getUserModel} from "@/app/models/userModel";
+import { getUserModel } from "@/app/models/userModel";
 
-export default function signIn(req, res){
-
-	handleRequest({req, res});
+export default function signIn(req, res) {
+  handleRequest({ req, res });
 }
 
+async function handleRequest(param) {
+  const { req, res } = param;
 
-async function handleRequest(param){
-	const {req, res} = param;
+  //get user
+  let user = null;
+  try {
+    const query = {
+      $or: [
+        { username: req.body.nameOrEmail },
+        { emailAddress: req.body.nameOrEmail },
+      ],
+    };
 
-	//get user
-	let user = null;
-	try{
-		const query = {
-			$or: [
-				{username: req.body.nameOrEmail},
-				{emailAddress: req.body.nameOrEmail},
-			],
-		}
+    user = await getUserModel(query);
+    console.log(user);
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "No user has been found:" });
+    }
 
-		user = await getUserModel(query);
-		if(!user){
-			return res.status(400).json({success: false, msg: "No user has been found:"});
-		}
+    const isCorrectPwd = await bcrypt.compare(req.body.password, user.password);
+    if (!isCorrectPwd) {
+      return res.status(400).json({ success: false, msg: "Wrong password." });
+    }
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ success: false, msg: "Internal server error." });
+  }
 
-		const isCorrectPwd = await bcrypt.compare(req.body.password, user.password);
-		if(!isCorrectPwd){
-			return res.status(400).json({success: false, msg: "Wrong password."});
-		}
-	}
-	catch(err){
-		console.log(err);
-		return res.status(500).json({success: false, msg: "Internal server error."});
-	}
+  //create token
+  let token = null;
+  console.log(user._id);
+  try {
+    token = jwt.sign(
+      {
+        _id: user._id,
+        username: user.username,
+        emailAddress: user.emailAddress,
+        password: user.password,
+      },
+      process.env.JWT_SECRET
+    );
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ success: false, msg: "Internal server error." });
+  }
 
-	//create token
-	let token = null;
-	try{
-		token = await jwt.sign(user, process.env.JWT_SECRET);
-	}
-	catch(err){
-		console.log(err);
-		return res.status(500).json({success: false, msg: "Internal server error."});
-	}
-
-	res.setHeader("Authorization", `Bearer ${token}`);
-	res.status(200).json({success: true, msg: "Authorization header set.", token});
+  res.setHeader("Authorization", `Bearer ${token}`);
+  res
+    .status(200)
+    .json({ success: true, msg: "Authorization header set.", token });
 }
